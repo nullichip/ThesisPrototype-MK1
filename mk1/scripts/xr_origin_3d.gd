7c047adb38c07696127a81014903ccdd35bbad8c
Rei <rei.huabci.yang@gmail.com>
2026-08-17 16:12:59 -0500

    update: detached script from root node and then also fixed errors in scripts.
    was able to work in VR.
    
    wip: moving around the room, controller/hand visibility, interactive books script + holding, new polycam models (possibly)

--- mk1/scripts/xr_origin_3d.gd
+++ mk1/scripts/xr_origin_3d.gd
@@ -5,7 +5,7 @@ var xr_interface : XRInterface
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	xr_interface = XRServer.find_interface("OpenXR")
-	if xr_interface and xr_interface.initialized():
+	if xr_interface and xr_interface.initialize():
 		print("OpenXR initialized successfully")
 		get_viewport().use_xr = true
 	else:
