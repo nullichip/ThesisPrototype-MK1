d4499365b85c11eac8c2bcb5a382a0c22dcd7a8f
R3iYaj <rei.huabci.yang@gmail.com>
2026-08-17 15:45:07 -0500

    update: reviewing script and adding light. got it to connect but then I spawned in a black void because the spawn point was in the bed mesh so I fixed that too.

--- mk1/scripts/interactive_items.gd
+++ mk1/scripts/interactive_items.gd
@@ -4,6 +4,6 @@ extends Area3D
 @export var haptic_amplitude : float = 0.5
 @export var duration : float = 0.1
 
-func on_area_entered(area_that_entered):
-	if area_that_entered.name == "LeftHandArea" or area_that_entered.name == "RightHandArea":
-		area_that_entered.get_parent().trigger_haptic_pulse("haptic", haptic_frequency, haptic_amplitude, duration, 0)
+func on_area_entered(area: Area2D):
+	if area.name == "LeftHandArea" or area.name == "RightHandArea":
+		area.get_parent().trigger_haptic_pulse("haptic", haptic_frequency, haptic_amplitude, duration, 0)
